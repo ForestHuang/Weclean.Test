7a196e44b02c7c37a6e2763f86e95abe13f41356
senlin.huang <senlin.huang@DESKTOP-DA4O6AC>
2018-06-03 21:59:42 +0800

    添加项目文件。

---
+++ Weclean.Test/HTTPCommon.cs
@@ -0,0 +1,60 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weclean.Test
+{
+    public class HTTPCommon
+    {
+        /// <summary>
+        /// Post
+        /// </summary>
+        /// <param name="restClient">RestClient</param>
+        /// <param name="dictionaryParamObj">参数</param>
+        /// <param name="methodName">方法名</param>
+        /// <returns>返回结果</returns>
+        public static string HttpPost(RestClient restClient, Dictionary<string, object> dictionaryParamObj, string methodName)
+        {
+            try
+            {
+                var request = new RestRequest(methodName, Method.POST);
+                request.RequestFormat = DataFormat.Json;
+                request.AddBody(dictionaryParamObj);
+                var _Response = restClient.Execute(request);
+                return _Response.Content;
+            }
+            catch (WebException ex)
+            {
+                return "无法连接到服务器\r\n错误信息：" + ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// GET
+        /// </summary>
+        /// <param name="restClient">RestClient</param>
+        /// <param name="methodName">方法名</param>
+        /// <param name="listParam">参数</param>
+        /// <returns>返回结果</returns>
+        public static string HttpGet(RestClient restClient, string methodName, List<string> listParam)
+        {
+            try
+            {
+                string paramObj = "/";
+                listParam.ForEach((x) => { paramObj += "{" + x + "}/"; });
+                var request = new RestRequest(methodName + paramObj.Substring(0, paramObj.Length - 1), Method.GET);
+                var response = restClient.Execute(request);
+                return response.Content;
+            }
+            catch (WebException ex)
+            {
+                return "无法连接到服务器\r\n错误信息：" + ex.Message;
+            }
+        }
+
+    }
+}
---
+++ Weclean.Test/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace Weclean.Test
+{
+    class Program
+    {
+
+        #region Private 方法
+
+        public static Stream FileToStream(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            byte[] bytes = new byte[fileStream.Length];
+            fileStream.Read(bytes, 0, bytes.Length);
+            fileStream.Close();
+            Stream stream = new MemoryStream(bytes);
+            return stream;
+        }
+
+        public static byte[] StreamToBytes(Stream stream)
+        {
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            stream.Seek(0, SeekOrigin.Begin);
+            return bytes;
+        }
+
+        #endregion
+
+        public static RestClient restClient = new RestClient("http://localhost:65519/WeClean/WeCleanBackstageService.svc");
+
+        static void Main(string[] args)
+        {
+
+            HTTPCommon.HttpGet(restClient, "WeCleanLogin", new List<string>() { "2", "a" });
+            string excelPath = @"D:\weclean.xlsx";
+            Stream stream = FileToStream(excelPath);
+            var model = JsonConvert.SerializeObject(new
+            {
+                CompanyName = "Weclean",
+                StreamFile = StreamToBytes(stream),
+                OperatorName = "senlin.huang"
+            });
+        }
+
+
+    }
+}
